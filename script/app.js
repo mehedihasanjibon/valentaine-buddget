94039751ed75cc531a506568f863b3d18d41aa01
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-14 17:25:34 +0600

    all js code done

--- script/app.js
+++ script/app.js
@@ -1,23 +1,52 @@
 document.getElementById("kitKat-btn").addEventListener('click', function(){
-    const quantity = document.getElementById("kitKat-quantity").value;
+    const quantity = getInputValue("kitKat-quantity");
     const kitKatCost = quantity * 200;
     setInnerText("chocolate", kitKatCost);
-    
+    total();
 });
 
 document.getElementById("rose-btn").addEventListener('click', function(){
-    const quantity = document.getElementById("rose-quantity").value;
+    // const quantity = document.getElementById("rose-quantity").value;
+    const quantity = getInputValue("rose-quantity")
     const roseCost = quantity * 100;
     setInnerText("rose", roseCost);
+    total();
 });
 
 document.getElementById("diary-btn").addEventListener("click", function(){
-    const quantity = document.getElementById("diary-quantity").value;
+    // const quantity = document.getElementById("diary-quantity").value;
+    const quantity = getInputValue("diary-quantity");
     const diaryCost = quantity * 300;
     setInnerText("diary", diaryCost)
+    total();
 })
 
 
 function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 }
+function getInputValue(id){
+    const value = document.getElementById(id).value;
+    return parseInt(value); 
+}
+
+function total(params){
+    const chocolate = document.getElementById("chocolate").innerText;
+    const rose = document.getElementById("rose").innerText;
+    const diary = document.getElementById("diary").innerText;
+    const totalSum = parseInt(chocolate)+ parseInt(rose) + parseInt(diary);
+    setInnerText("total", totalSum);
+}
+
+document.getElementById("promoApplyBtn").addEventListener("click",function(){
+    const promoCode = getInputValue("promo-code");
+    if(promoCode === 101){
+        const total = document.getElementById("total").innerText;
+        const sum = total - parseInt(total) * 0.1;
+        setInnerText("all-total", sum);
+    }
+    else{
+        alert("wrong promo code try again with valid code");
+    }
+
+})
